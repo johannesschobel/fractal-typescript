21729665330bed635be8df5b56c6842351e0333f
Daniel <dakesi95@gmail.com>
2019-06-12 15:05:35 +0200

    Refactoring of transformer

---
+++ src/BookTransformer.ts
@@ -0,0 +1,32 @@
+import {Book} from "../models/Book";
+
+export class BookTransformer {
+
+    public static transformer = (books: Array<Book>) => {
+        let result: Array<any> = [];
+        for (const book of books) {
+            result.push({
+                "id": book.id,
+                "title": book.title,
+                "year": book.yr,
+                "author": {
+                    "name": book.author_name,
+                    "email": book.author_email
+                },
+                "links": {
+                    "rel": "self",
+                    "uri": "/books/" + book.id
+                }
+            });
+        }
+        return result;
+    };
+
+    public transform(book: Book){
+        return {
+            "id": book.id,
+            "title": book.title
+        }
+    }
+
+}
--- src/FractalTypescript.ts
+++ src/FractalTypescript.ts
@@ -1,32 +1,13 @@
 import {Book} from "../models/Book";
+import {BookTransformer} from "./BookTransformer";
 
 export class FractalTypescript {
 
-    private transformer = (books: Array<Book>) => {
-        let result: Array<any> = [];
-        for (const book of books) {
-            result.push({
-                "id": book.id,
-                "title": book.title,
-                "year": book.yr,
-                "author": {
-                    "name": book.author_name,
-                    "email": book.author_email
-                },
-                "links": {
-                    "rel": "self",
-                    "uri": "/books/" + book.id
-                }
-            });
-        }
-        return result;
-    };
-
-    public transformData(data: Array<any>, option: string) {
+    public transformData(data: Array<Book>, option: string) {
         if (option === "array") {
-            return this.transformer(data);
+            return BookTransformer.transformer(data);
         } else if (option === "json") {
-            let result = this.transformer(data);
+            let result = BookTransformer.transformer(data);
             return JSON.stringify(result);
         }
     }

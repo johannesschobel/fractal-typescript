6c7b477e65e5d23fcc779191949470a357b77df8
Paul Romahn <promahn@outlook.de>
2019-06-18 13:24:29 +0200

    Fixed Build fail

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
-import {Book} from "./models/Book";
-import {Manager} from "./Manager";
-import {Collection} from "./resource/Collection";
+import {Book} from './models/Book';
+import {Manager} from './Manager';
+import {Collection} from './resource/Collection';
 
 let fractal = new Manager(undefined);
 
 let books: Array<Book> = [
     {
-        "id": "1",
-        "title": "Hogfather",
-        "yr": 1998,
-        "author_name": "Philip K Dick",
-        "author_email": "philip@example.org",
+        'id': '1',
+        'title': 'Hogfather',
+        'yr': 1998,
+        'author_name': 'Philip K Dick',
+        'author_email': 'philip@example.org',
     },
     {
-        "id": "2",
-        "title": "Game Of Kill Everyone",
-        "yr": 2014,
-        "author_name": "George R. R. Satan",
-        "author_email": "george@example.org",
+        'id': '2',
+        'title': 'Game Of Kill Everyone',
+        'yr': 2014,
+        'author_name': 'George R. R. Satan',
+        'author_email': 'george@example.org',
     }
 ];
 
 let resource = new Collection(books, function(book: Book){
    return {
-       "id": book.id,
-       "title": book.title,
-       "year": book.yr,
-       "author": {
-           "name": book.author_name,
-           "email": book.author_email
+       id: book.id,
+       title: book.title,
+       year: book.yr,
+       author: {
+           name: book.author_name,
+           email: book.author_email
        },
-       "links": {
-           "rel": "self",
-           "uri": "/books/" + book.id
+       link: {
+           rel: 'self',
+           uri: '/books/' + book.id
        }
    };
 });

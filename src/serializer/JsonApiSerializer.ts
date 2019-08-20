6136884bb2393f9bfc04e75e4e971ebaa6142929
Daniel Kempf <dakesi95@gmail.com>
2019-08-20 17:14:17 +0200

    Further implementation of JsonApiSerializer

--- src/serializer/JsonApiSerializer.ts
+++ src/serializer/JsonApiSerializer.ts
@@ -13,30 +13,112 @@ export class JsonApiSerializer extends ArraySerializer {
     }
 
     public collection(resourceKey: string, data: {}): {} {
-        const resources = [] as any[];
+        let resources = [] as any[];
 
         for (const resource of Object.keys(data)) {
+            // @ts-ignore
+            resources = this.item(resourceKey, resource).data;
         }
 
         return {data: resources};
     }
 
-    public item(resourceKey: string, data: {}): {} {
+    public item(resourceKey: string, data: {meta: {}, links: {}}): {} {
         const id = this.getIdFromData(data);
 
-        // todo
+        const resource = {
+            data: {
+                attributes: data,
+                id,
+                type: resourceKey
+            }
+        };
 
-        return {};
+        // @ts-ignore
+        delete resource.data.attributes.id;
+
+        let customLinks: {};
+        if (resource.data.attributes.links !== undefined) {
+            customLinks = data.links;
+            delete resource.data.attributes.links;
+        }
+
+        if (resource.data.attributes.meta !== undefined) {
+            // @ts-ignore
+            resource.data.meta = data.meta;
+            delete resource.data.attributes.meta;
+        }
+
+        const attributes = resource.data.attributes;
+        if (Object.entries(attributes).length === 0 && attributes.constructor === Object) {
+            // @ts-ignore
+            resource.data.attributes = {};
+        }
+
+        if (this.shouldIncludeLinks()) {
+            // @ts-ignore
+            resource.data.links = {
+                self: this.baseUrl + '/' + resourceKey + '/' + id
+            };
+            if (customLinks !== undefined) {
+                // @ts-ignore
+                resource.data.links = {...resource.data.links, ...customLinks}
+            }
+        }
+
+        return resource;
     }
 
     public paginator(paginator: PaginatorInterface): {} {
-        // todo
-        return {};
+        const currentPage = paginator.getCurrentPage();
+        const lastPage = paginator.getLastPage();
+
+        const pagination = {
+            count: paginator.getCount(),
+            current_page: currentPage,
+            links: {
+                first: '',
+                last: '',
+                next: '',
+                prev: '',
+                self: ''
+            },
+            per_page: paginator.getPerPage(),
+            total: paginator.getTotal(),
+            total_pages: lastPage
+        };
+
+        pagination.links.self = paginator.getUrl(currentPage);
+        pagination.links.first = paginator.getUrl(1);
+
+        if (currentPage > 1) {
+            pagination.links.prev = paginator.getUrl(currentPage - 1);
+        }
+
+        if (currentPage < lastPage) {
+            pagination.links.next = paginator.getUrl(currentPage + 1);
+        }
+
+        pagination.links.last = paginator.getUrl(lastPage);
+        return {pagination};
     }
 
     public meta(meta: {}): {} {
-        // todo
-        return {};
+        if (meta === undefined) {
+           return {};
+        }
+
+        const result = { meta };
+
+        // @ts-ignore
+        if (result.meta.pagination !== undefined) {
+           // @ts-ignore
+            result.links = result.meta.pagination.links;
+           // @ts-ignore
+            delete result.meta.pagination.links;
+        }
+
+        return result;
     }
 
     public null(): {} {
@@ -44,7 +126,12 @@ export class JsonApiSerializer extends ArraySerializer {
     }
 
     public includedData(resource: ResourceInterface, data: {}): {} {
+        const pullOutNestedIncludedData = this.pullOutNestedIncludedData(data);
+        const serializedData = pullOutNestedIncludedData[0];
+        const linkedIds = pullOutNestedIncludedData[1];
+
         // todo
+
         return {};
     }
 
@@ -115,9 +202,11 @@ export class JsonApiSerializer extends ArraySerializer {
         return 0;
     }
 
-    protected pullOutNestedIncludedData(data: {}): {} {
+    protected pullOutNestedIncludedData(data: {}): any[] {
         // todo
-        return {};
+        const includedData = {};
+        const linkedIds = {};
+        return [includedData, linkedIds];
     }
 
     protected shouldIncludeLinks(): boolean {

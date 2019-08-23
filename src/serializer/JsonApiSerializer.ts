56aedc188fda84a5cfa23075f676e951dfd2336b
Daniel Kempf <dakesi95@gmail.com>
2019-08-23 11:45:41 +0200

    Created first shot of JsonApiSerializer

--- src/serializer/JsonApiSerializer.ts
+++ src/serializer/JsonApiSerializer.ts
@@ -4,12 +4,12 @@ import {ArraySerializer} from '../../src/serializer/ArraySerializer';
 
 export class JsonApiSerializer extends ArraySerializer {
     protected baseUrl: string;
-    protected rootObjects: {};
+    protected rootObjects: [];
 
     constructor(baseUrl: string = null) {
         super();
         this.baseUrl = baseUrl;
-        this.rootObjects = {};
+        this.rootObjects = [];
     }
 
     public collection(resourceKey: string, data: {}): {} {
@@ -127,134 +127,298 @@ export class JsonApiSerializer extends ArraySerializer {
 
     public includedData(resource: ResourceInterface, data: {}): {} {
         const pullOutNestedIncludedData = this.pullOutNestedIncludedData(data);
-        const serializedData = pullOutNestedIncludedData[0];
-        const linkedIds = pullOutNestedIncludedData[1];
+        let serializedData = pullOutNestedIncludedData[0];
+        let linkedIds = pullOutNestedIncludedData[1];
 
-        // todo
+        for (const key of Object.keys(data)) {
+            // @ts-ignore
+            if (this.isNull((data[key])) || this.isEmpty(data[key])) {
+                continue;
+            }
 
-        return {};
+            // @ts-ignore
+            const includeObjects = this.createIncludeObjects(data[key]);
+            const serializeIncludedObjectsWithCacheKey = this.serializeIncludedObjectsWithCacheKey(includeObjects,
+                linkedIds, serializedData);
+            serializedData = serializeIncludedObjectsWithCacheKey[0];
+            linkedIds = serializeIncludedObjectsWithCacheKey[1];
+        }
+
+        return serializedData === null ? {} : {included: serializedData};
     }
 
     public sideloadIncludes(): boolean {
         return true;
     }
 
-    public injectData(data: any[], rawIncludedData: any[]): any[] {
-        // todo
-        return [];
+    public injectData(data: [], includedData: any[]): [] {
+        const relationship = this.parseRelationships(includedData);
+
+        if (relationship !== undefined) {
+            data = this.fillRelationships(data, relationship);
+        }
+        return data;
     }
 
     public filterIncludes(includedData: any[], data: any[]): any[] {
-        // todo
-        return [];
+        // @ts-ignore
+        if (includedData.included !== undefined) {
+            return includedData;
+        }
+
+        this.createRootObjects(data);
+
+        // @ts-ignore
+        includedData.included = includedData.included.filter((key) => key !== 'filterRootObject');
+
+        return includedData;
     }
 
     public getMandatoryFields(): string[] {
         return ['id'];
     }
 
-    public injectAvailableIncludeData(data: {}, availableIncludes: any): any {
-        // todo
-        return;
+    public injectAvailableIncludeData(data: {}, availableIncludes: []): any {
+        if (!this.shouldIncludeLinks()) {
+            return data;
+        }
+
+        if (this.isCollection(data)) {
+            let resource = {};
+            for (const relationshipKey of availableIncludes) {
+                resource = this.addRelationsipLinks(resource, relationshipKey);
+            }
+            // @ts-ignore
+            data.data = resource;
+        } else {
+            for (const relationshipKey of availableIncludes) {
+                // @ts-ignore
+                data.data = this.addRelationsipLinks(data.data, relationshipKey);
+            }
+        }
+        return data;
     }
 
     protected filterRootObject(object: {}): boolean {
-        // todo
-        return true;
+        return !this.isRootObject(object);
     }
 
     protected setRootObjects(objects: []): void {
-        // todo
+        for (const object of objects) {
+            const rootObject = {};
+            // @ts-ignore
+            const type = object.type;
+            // @ts-ignore
+            rootObject[type] = object.id;
+            // @ts-ignore
+            this.rootObjects.push(rootObject);
+        }
     }
 
     protected isRootObject(object: {}): boolean {
-        // todo
-        return false;
+        // @ts-ignore
+        return this.rootObjects.includes(object);
     }
 
     protected isCollection(data: {}): boolean {
-        // todo
-        return true;
+        return Array.isArray(data);
     }
 
     protected isNull(data: {}): boolean {
-        // todo
-        return false;
+        // @ts-ignore
+        return data.data === null;
     }
 
     protected isEmpty(data: {}): boolean {
-        // todo
-        return false;
+        // @ts-ignore
+        return data.data === {};
     }
 
-    protected fileRelationships(data: {}, relationship: []): {} {
-        // todo
-        return {};
+    protected fillRelationships(data: {}, relationships: []): [] {
+        let relationshipsResult = [] as [];
+        if (this.isCollection(data)) {
+            for (const key of Object.keys(relationships)) {
+                // @ts-ignore
+                relationshipsResult = this.fillRelationshipAsCollection(data, relationships[key], key);
+            }
+        } else {
+            for (const key of Object.keys(relationships)) {
+                // @ts-ignore
+                relationshipsResult = this.fillRelationshipAsSingleResource(data, relationships[key], key)
+            }
+        }
+        return relationshipsResult;
     }
 
-    protected parseRelationships(includedData: {}): {} {
-        // todo
-        return {};
+    protected parseRelationships(includedData: {}): [] {
+        let relationships = [] as [];
+        for (const key of Object.keys(includedData)) {
+            // @ts-ignore
+            relationships = this.buildRelationship(key, relationships, includedData[key], key);
+            // @ts-ignore
+            if (includedData.includedKey.meta !== undefined) {
+                // @ts-ignore
+                relationships[key].meta = includedData[key].meta;
+            }
+        }
+        return relationships;
     }
 
     protected getIdFromData(data: {}): number {
-        // todo
-        return 0;
+        // @ts-ignore
+        if (data.id === undefined) {
+            throw new Error('JSON API resource objects MUST have a valid id');
+        }
+        // @ts-ignore
+        return data.id;
     }
 
     protected pullOutNestedIncludedData(data: {}): any[] {
-        // todo
         const includedData = {};
-        const linkedIds = {};
+        const linkedIds = [] as [];
+
+        for (const key of Object.keys(data)) {
+            // @ts-ignore
+            if (data[key].included !== undefined) {
+                // @ts-ignore
+                this.serializeIncludedObjectsWithCacheKey(data[key].included, linkedIds, includedData);
+            }
+        }
+
         return [includedData, linkedIds];
     }
 
     protected shouldIncludeLinks(): boolean {
-        // todo
-        return false;
+        return this.baseUrl !== null;
     }
 
     private createIncludeObjects(includeObject: {}): {} {
-        // todo
-        return {};
+        let includeObjects = [] as [];
+
+        if (this.isCollection(includeObject)) {
+            // @ts-ignore
+            includeObjects = includeObject.data;
+            return includeObjects;
+        } else  {
+            // @ts-ignore
+            includeObjects = [].push(includeObjects.data);
+            return includeObjects;
+        }
     }
 
-    private createRootObject(data: {}): void {
-        // todo
+    private createRootObjects(data: {}): void {
+        // @ts-ignore
+        this.setRootObjects(data.data);
     }
 
-    private fileRelationshipAsCollection(data: {}, relationship: {}, key: string): {} {
-        // todo
-        return {};
+    private fillRelationshipAsCollection(data: {}, relationship: {}, key: string): {} {
+        for (const relationKey of Object.keys(relationship)) {
+            // @ts-ignore
+            data.data[relationKey].relationships[key] = relationship;
+        }
+        return data;
     }
 
-    private fillRelationshipAsSingleResource(data: {}, relationship: {}, key: string) {
-        // todo
-        return {};
+    private fillRelationshipAsSingleResource(data: {}, relationship: {}, key: string): {} {
+        // @ts-ignore
+        data.data.relationships[key] = relationship;
+        return data;
     }
 
-    private buildRelationship(inlcudeKey: string, relationsip: {}, includeObject: {}, key: string): {} {
-        // todo
-        return {};
+    private buildRelationship(includeKey: string, relationships: {}, includeObject: {}, key: string): {} {
+        // @ts-ignore
+        let relationshipsModified = this.addIncludeKeyToRelationsIfNotSet(includeKey, relationships);
+
+        if (this.isNull(includeObject)) {
+            relationshipsModified = this.null();
+        } else if (this.isEmpty(includeObject)) {
+            relationshipsModified = {
+                data: []
+            };
+        } else if (this.isCollection(includeObject)) {
+            relationshipsModified = {
+                data: []
+            }
+        } else {
+            // @ts-ignore
+            const id = includeObject.data.id;
+            // @ts-ignore
+            const type = includeObject.data.type;
+            relationshipsModified = {
+                data: {
+                    id,
+                    type
+                }
+            }
+        }
+
+        // @ts-ignore
+        relationshipsModified[includeKey][key] = relationshipsModified;
+        return relationshipsModified;
     }
 
-    private addIncludeKeyToRElationsIfNotSet(includeKey: string, relationship: {}): {} {
-        // todo
-        return {};
+    private addIncludekeyToRelationsIfNotSet(includeKey: string, relationship: {}): {} {
+        // @ts-ignore
+        if (relationship[includeKey] === undefined) {
+            // @ts-ignore
+            relationship[includeKey] = '';
+            return relationship;
+        }
+        return relationship;
     }
 
     private addIncludedDataToRelationship(includeObject: {}, relationship: {}): {} {
-        // todo
-        return {};
+        // @ts-ignore
+        for (const object of Object.keys(includeObject.data)) {
+            // @ts-ignore
+            const type = object.type;
+            // @ts-ignore
+            const id = object.id;
+            // @ts-ignore
+            relationship.data = {
+                id,
+                type
+            }
+        }
+        return relationship;
     }
 
     private addRelationsipLinks(resource: {}, relationshipKey: string): {} {
-        // todo
-        return {};
+        // @ts-ignore
+        if (resource.relationships !== undefined || resource.relationships[relationshipKey] !== undefined) {
+            // @ts-ignore
+            resource.relationsships[relationshipKey] = [];
+        }
+
+        // @ts-ignore
+        const type = resource.type;
+        // @ts-ignore
+        const id = resource.id;
+        const links = {
+          links: {
+              related: this.baseUrl + '/' + type + '/' + id + '/' + relationshipKey,
+              self: this.baseUrl + '/' + type + '/' + id + '/relationships' + relationshipKey
+          }
+        };
+        // @ts-ignore
+        resource.relationships[relationshipKey] = {...links, ...resource.relationships[relationshipKey]};
+        return resource;
     }
 
-    private serializeIncludedObjectsWithCacheKey(includedObjects: {}, linkedIds: [], serializedData: {}): {} {
-        // todo
-        return {};
+    private serializeIncludedObjectsWithCacheKey(includedObjects: {}, linkedIds: [], serializedData: {}): [] {
+        for (const object of Object.keys(includedObjects)) {
+            // @ts-ignore
+            const includeType = object.type;
+            // @ts-ignore
+            const includeId = object.id;
+            const cacheKey = includeType.includeId;
+            if (linkedIds[cacheKey] === undefined) {
+                serializedData = object;
+                // @ts-ignore
+                linkedIds[cacheKey] = object;
+            }
+        }
+        // @ts-ignore
+        return [serializedData, linkedIds];
     }
 }
